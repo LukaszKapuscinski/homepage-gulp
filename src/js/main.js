5788f6d2289e32c1a97c5b2761984c8a4397e988
Lukasz Kapuscinski <lukasz.dawid.kapuscinski@gmail.com>
2019-07-13 10:54:23 +0100

    add week content

--- src/js/main.js
+++ src/js/main.js
@@ -17,13 +17,13 @@ welcome(myName, myAge);
 welcome('Lukasz',31);
 
 
-const button = document.querySelector('.header__button--js');
+const button = document.querySelector('.footer__button--js');
 console.log(button);
 
 
 
 button.addEventListener('click', (e)=>{
-const header = document.querySelector('.header__title--js');
+const header = document.querySelector('.footer__title--js');
 header.innerHTML ='klik, klik';
-header.classList.toggle('header__title--red');
+header.classList.toggle('footer__title--red');
 });
