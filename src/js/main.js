d729cd5ac986b61adab69be02471df304a3ebab9
Lukasz Kapuscinski <lukasz.dawid.kapuscinski@gmail.com>
2019-08-03 13:48:12 +0100

    add variables in SCSS

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,6 @@
-
 import moment from 'moment';
 
-console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
-
-
-
+ console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
 
 const welcome = (name, age) => {
     console.log( `Witaj ${name}, masz ${age} lat`);
